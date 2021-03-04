deb8c786e84d2dcd9eb54f9a6ac23c47585cb9fc
Hongjun Yun <46326335+andylang8445@users.noreply.github.com>
2021-03-04 18:28:43 -0500

    .

--- GoogleFormFiller/main.py
+++ GoogleFormFiller/main.py
@@ -12,7 +12,7 @@ def checkin():
 
     browser = webdriver.Chrome(executable_path='/users/hongjunyun/chromedriver', options=option)
 
-    browser.get("")
+    browser.get("https://forms.gle/LLUCAY2GEoyAAL4m9")
 
     date = browser.find_elements_by_class_name("quantumWizTextinputPaperinputInput")
     radiobuttons = browser.find_elements_by_class_name("docssharedWizToggleLabeledLabelWrapper")
